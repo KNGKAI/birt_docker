5114e970d9eee0053806bca246d78762cfadac1c
Niel Jordaan <nielj@imrsnt.com>
2020-06-30 10:13:50 +0200

    found copying to container work around, volumes make read only

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-# birt
-#docker run --rm --name birt-viewer -v $(pwd)/report:/usr/local/tomcat/webapps/birt/report -d psimonov/birt-viewer
+# docker run --rm --name birt-viewer -v $(pwd)/report/:/usr/local/tomcat/webapps/birt/report/ -d birt-viewer
 
 docker run --rm -p 8080:8080 --name birt-viewer -d birt-viewer
+
+sleep 5
+
+docker cp $(pwd)/report/. $(docker ps -lq):/usr/local/tomcat/webapps/birt/report/
